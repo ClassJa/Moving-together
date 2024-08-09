912e6594176331a64f3d41e8748116d41737339b
JA <69608172+ClassJay@users.noreply.github.com>
2024-08-08 20:20:04 -0500

    working on signInRoutes javascript

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const userRoutes = require('./userRoutes');
 const jobRoutes = require('./jobRoutes');
 const projectRoutes = require('./projectRoutes');
 const testimonialRoutes = require('./testimonialRoutes');
+const signInRoutes = require('./signInRoutes')
 
 // When a request is made to the /users or /projects path, it will be directed to the index.js in the /users or /projects folder.
 router.use('/users', userRoutes);
 router.use('/jobs', jobRoutes);
 router.use('/projects', projectRoutes);
 router.use('/testimonials', testimonialRoutes)
+router.use('/login', signInRoutes);
 
 module.exports = router;
